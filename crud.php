cecd67f2ae51eb7b8d4bd7ea5dfc4459526e5d16
Bernardo Colorado Dubois <33713253+HardCoreWare@users.noreply.github.com>
2018-06-22 22:32:55 -0500

    Update crud.php

--- crud.php
+++ crud.php
@@ -4,18 +4,25 @@ class sql_crud{
 
     private $connection;
 
-    // just creates a new connection of mySQL from sqli
+// just creates a new connection of mySQL from sqli
 
     public function __construct($host,$user,$password,$database) {
 
-        $this->connection = new mysqli($host,$user,$password,$database);
+        try{
+
+            $this->connection = new mysqli($host,$user,$password,$database);
+
+        }
+
+            catch(Throwable $exception){
+
+        }
 
     }
 
 //-------------------------------------------------------------------------------------------------------------------------
 
-
-    // get the status of connection
+// get the status of connection
 
     public function get_status(){
 
@@ -35,46 +42,57 @@ class sql_crud{
 
 //-------------------------------------------------------------------------------------------------------------------------
 
-    /*validates a password and username and returns
+/*validates a password and username and returns
 
-    0 invalid username
+-1 error
 
-    1 valid username but invalid password
+ 0  invalid username
 
-    2 valid username and password
+ 1  valid username but invalid password
 
-    */
+ 2  valid username and password
+
+*/
 
     public function validate_password($table,$name_col,$password_col,$name,$password){
 
         $q = "select ".$name_col." , ".$password_col." from ".$table." where ".$name_col." = '".$name."'";
 
-        $result = $this->connection->query($q);
+        try{
 
-        if($result->num_rows > 0){
+            $result = $this->connection->query($q);
 
-            if($row = $result-> fetch_assoc()){
+            if($result->num_rows > 0){
 
-                $_name=$row[$name_col];
+                if($row = $result-> fetch_assoc()){
 
-                $_password=$row[$password_col];
+                    $_name=$row[$name_col];
 
-                if($password == $_password){
+                    $_password=$row[$password_col];
 
-                    return 2;
+                    if($password == $_password){
+
+                        return 2;
+
+                    }
+
+                    else{
+
+                        return 1;
+
+                    }
 
                 }
+      
+                else {
 
-
-                else{
-
-                    return 1;
+                    return 0;
 
                 }
 
             }
-            
-            else {
+
+            else{
 
                 return 0;
 
@@ -82,13 +100,14 @@ class sql_crud{
 
         }
 
-        else{
+        catch(Throwable $exception){
 
-            return 0;
+            return -1;
 
         }
 
     }
+
 
 //---------------------------------------------------------------------------------------------------------------------------
  
@@ -98,29 +117,41 @@ returns single line or the first line of a query as an array of strings
 
 in the case of non existing lines the function will return an array of 2 null strings
 
+e = 1 gives the echo of an exception
+
+e = 0 doesn't gives echo
+
 */
 
-function get_line($q){
+    public function get_line($q){
 
-    $result = $this->connection->query($q);
+        $result = $this->connection->query($q);
 
-    if($result->num_rows > 0){
+        try{
 
-        $line=$result->fetch_array(MYSQLI_NUM);
+            if($result->num_rows > 0){
 
-        return $line;
+                $line=$result->fetch_array(MYSQLI_NUM);
+
+                return $line;
+
+            }
+
+            else {
+
+                $null_line=array("","");
+
+                return $null_line;
+
+            }
+
+        }
+
+        catch(Throwable $exception){
+
+        }
 
     }
-
-    else {
-
-        $null_line=array("","");
-
-        return $null_line;
-
-    }
-
-}
 
 //---------------------------------------------------------------------------------------------------------------------------------
 
@@ -130,26 +161,43 @@ returns the full table of a query as a string matrix
 
 in the case of non existing lines the function will return a matrix of 2x2 null strings
 
+e = 1 gives the echo of an exception
+
+e = 0 doesn't gives echo
+
 */
 
-    function get_table($q){
+    public function get_table($q){
 
-        $result = $this->connection->query($q);
+
+        try{
+
+            $result = $this->connection->query($q);
     
-        if($result->num_rows > 0){
+            if($result->num_rows > 0){
     
-            $table=$result->fetch_all(MYSQLI_NUM);
+                $table=$result->fetch_all(MYSQLI_NUM);
     
-            return $table;
+                return $table;
     
+            }
+    
+            else {
+    
+                $null_table=array(array("",""),array("",""));
+
+                return $null_table;
+    
+            }
+
         }
-    
-        else {
-    
+
+        catch(Throwable $exception){
+
             $null_table=array(array("",""),array("",""));
 
             return $null_table;
-    
+
         }
 
     }
@@ -162,23 +210,38 @@ returns the selected cell as a string or the cell at index 0,0 of the query
 
 or in the case of a null result the function returns a null string
 
+e = 1 gives the echo of an exception
+
+e = 0 doesn't gives echo
+
 */ 
 
     public function get_cell($q){
 
-        $result = $this->connection->query($q);
 
-        if($result->num_rows > 0){
+        try{
 
-            $line=$result->fetch_array(MYSQLI_NUM);
+            $result = $this->connection->query($q);
 
-            $cell=$line[0];
+            if($result->num_rows > 0){
+
+                $line=$result->fetch_array(MYSQLI_NUM);
+
+                $cell=$line[0];
     
-            return $cell;
+                return $cell;
     
+            }
+
+            else{
+
+                return "";
+
+            }
+
         }
 
-        else{
+        catch(Throwable $exception){
 
             return "";
 
@@ -186,44 +249,86 @@ or in the case of a null result the function returns a null string
 
     }
 
+//--------------------------------------------------------------------------------------------------------------------------
+
 /*
 
 returns single column or the first column of a query as an array of strings
 
 in the case of non existing lines the function will return an array of 2 null strings
 
+e = 1 gives the echo of an exception
+
+e = 0 doesn't gives echo
+
 */
 
-    function get_col($q){
+    public function get_col($q){
 
-        $result = $this->connection->query($q);
+        try{
+
+            $result = $this->connection->query($q);
     
-
-
-        if($result->num_rows > 0){
+            if($result->num_rows > 0){
     
-            $table=$result->fetch_all(MYSQLI_NUM);
+                $table=$result->fetch_all(MYSQLI_NUM);
 
-            $s=sizeof($table);
+                $s=sizeof($table);
 
-            $col=array("","");
+                $col=array("","");
 
-            for($i=0; $i<$s; $i++){
+                for($i=0; $i<$s; $i++){
 
-                $col[$i]=$table[$i][0];
+                    $col[$i]=$table[$i][0];
 
+                }
+
+                return $col;
+    
+            }
+    
+            else {
+    
+                $null_col=array("","");
+
+                return $null_col;
+    
             }
 
-            return $col;
-    
         }
-    
-        else {
-    
+
+        catch(Throwable $exception) {
+
             $null_col=array("","");
 
             return $null_col;
-    
+
+        }
+
+    }
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+/*
+
+e = 1 gives the echo of an exception
+
+e = 0 doesn't gives echo
+
+*/
+
+    public function update($q){
+
+        try{
+
+            $result = $this->connection->query($q);
+
+        }
+
+        catch(Throwable $exception){
+
+            echo($exception);
+
         }
 
     }
